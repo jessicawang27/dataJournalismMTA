a5c2af49c55d915b1c7d9fdd626a3b02d4fea730
Jessica Wang <jessica.wang27@trinityschoolnyc.org>
2025-05-14 13:35:01 -0400

    start server.py

--- data/format_data.py
+++ data/format_data.py
@@ -4,15 +4,26 @@ f1 = open("subway_data_cleansed.csv", "r")
 lines = f1.readlines()
 
 dictionary = {
-    'borough' : [],
-    'stop' : []
+    'M' : [],
+    'B' : [],
+    'Q' : [],
+    'Bx' : [],
+    'SI' : []
 }
 
 
 for i in range(1, len(lines)):
     line = lines[i].replace("\n","").split(",")
-    dictionary['borough'].append(line[0])
-    dictionary['stop'].append(line[1])
+    if line[0] == 'M':
+        dictionary['M'].append(line[1])
+    elif line[0] == 'B':
+        dictionary['B'].append(line[1])
+    elif line[0] == 'Q':
+        dictionary['Q'].append(line[1])
+    elif line[0] == 'Bx':
+        dictionary['Bx'].append(line[1])
+    else: # Staten Island
+        dictionary['SI'].append(line[1])
 
 f1.close()
 
