9e985a5226d4771fd4fcc2e19d239822f5a7b01f
jessicawang27 <162509898+jessicawang27@users.noreply.github.com>
2025-05-27 00:46:18 -0400

    update MS1

--- data/format_data.py
+++ data/format_data.py
@@ -1,9 +1,9 @@
 import json
 
 f1 = open("subway_data_cleansed.csv", "r")
-lines = f1.readlines()
+lines = f1.readlines()[1:]
 
-dictionary = {
+boroughs = {
     'M' : [],
     'B' : [],
     'Q' : [],
@@ -11,23 +11,32 @@ dictionary = {
     'SI' : []
 }
 
+shorthand = {
+    'M' : 'Manhattan',
+    'B' : 'Brooklyn',
+    'Q' : 'Queens',
+    'Bx' : 'The Bronx',
+    'SI' : 'Staten Island'
+}
 
 for i in range(1, len(lines)):
     line = lines[i].replace("\n","").split(",")
-    if line[0] == 'M':
-        dictionary['M'].append(line[1])
-    elif line[0] == 'B':
-        dictionary['B'].append(line[1])
-    elif line[0] == 'Q':
-        dictionary['Q'].append(line[1])
-    elif line[0] == 'Bx':
-        dictionary['Bx'].append(line[1])
-    else: # Staten Island
-        dictionary['SI'].append(line[1])
+    boroughs[line[0]].append(line[1])
 
 f1.close()
 
-# #Save the json object to a file
-f2 = open("subway_data.json", "w")
-json.dump(dictionary, f2, indent = 4)
-f2.close()
+subways = {}
+for line in lines:
+    infos = line.split(",")
+    share_lines = infos[2].replace("\n","").split(" ")
+    for line in share_lines:
+        if line not in subways.keys():
+            subways[line] = [infos[1]]
+        elif infos[1] not in subways[line]:
+            subways[line].append(infos[1])
+
+data = {
+    'boroughs':boroughs,
+    'shorthand':shorthand,
+    'subways':subways
+}
